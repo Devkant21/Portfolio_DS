25fc95ab6c6119fa4c9691fc90a392d969c7c15a
Devkant <devkant21@gmail.com>
2022-03-29 17:19:41 +0530

    Photo uploaded

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,15 +9,15 @@ const Navbar = () => {
     <div className='flex flex-col md:flex-row md:justify-center justify-center items-center py-2 shadow-xl'>
         <div className='logo mx-5'>
             <Link href={'/'}><a>
-                <Image width={40} height={40} src="/codedamn_logo.png" alt="" />
+                <Image width={40} height={40} src="/dev.jpeg" alt="" />
                 </a>
             </Link>
             
         </div>
             <div className='nav'>
                 <ul className='flex items-center space-x-4 font-bold md:text-md'> 
-                    <Link href='https://codedamn.com/playgrounds'><a><li>Portfolio</li></a></Link>
-                    <Link href={'Blog'}><a><li>Blog</li></a></Link>
+                    <Link href={'Portfolio'}><a><li>Portfolio</li></a></Link>
+                    <Link href='https://dev21.hashnode.dev/'><a><li>Blog</li></a></Link>
                 </ul>
             </div>
             <div className='AiFillGithub absolute right-0 top-4 mx-5 cursor-pointer'>
