d05145b10f426b190189fb50bc980c8d1f2797c1
Devkant <devkant21@gmail.com>
2022-03-31 18:52:43 +0530

    footer link added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div>
-            <footer className="text-gray-600 body-font">
+            <footer className="text-gray-600 bottom-0 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
             <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                 
                 <span className="ml-3 text-xl">DSwargiary</span>
             </a>
             <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2022 All Rights Reserved —
-                <a href="https://github.com/Devkant21" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@Devkant Swargiary</a>
+                <Link href="https://github.com/Devkant21" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@Devkant Swargiary</Link>
             </p>
             </div>
             </footer>
